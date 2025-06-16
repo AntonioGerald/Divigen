e7321736b283de60ba29bfe8c4d75d9b9b4c1ef7
Petra Antonio <nightforces351@gmail.com>
2025-06-16 23:18:32 +0700

    commit

--- src/app/public-umkms/page.tsx
+++ src/app/public-umkms/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = 'force-dynamic'; 
+
 import React from "react";
 import { useAppContext } from "@/context/AppContext"; // Perbaikan impor
 import Link from "next/link";
